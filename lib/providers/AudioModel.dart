8a9641b8dfb0418a4dbbfe29c12dd052fffd7980
veerse <unileven95@gmail.com>
2020-03-30 10:28:04 +0200

    notification audio

--- lib/providers/AudioModel.dart
+++ lib/providers/AudioModel.dart
@@ -19,10 +19,23 @@ class AudioModel extends ChangeNotifier {
   }
 
   void play() async {
-    await audioPlayer.play("http://93.6.197.182:8095/audio/1/download", isLocal: false).then((v){
+    await audioPlayer.play("https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_1MG.mp3", isLocal: false).then((v){
       print('Playing $v');
     }).catchError((e){
       print('error $e');
+    });
+
+
+
+    audioPlayer.setNotification(
+      title: 'Test audio',
+      artist: 'The artist',
+      albumTitle: 'The album',
+      imageUrl: 'https://i.picsum.photos/id/59/200/200.jpg'
+    ).then((v){
+      print('Notification ok');
+    }).catchError((e){
+      print('error with setNotification $e');
     });
   }
 
